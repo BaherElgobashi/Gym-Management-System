2d3d25f1a268020c81f2ddcc6ad39c81cff30bc4
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-15 14:58:28 +0200

    Add Properties To GymUser Entity.

--- GymManagementDAL/Entities/GymUser.cs
+++ GymManagementDAL/Entities/GymUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GymManagementDAL.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,10 @@ namespace GymManagementDAL.Entities
 {
     internal class GymUser : BaseEntity
     {
-        public string Name { get; set; } = null;
-        public string Email { get; set; } = null;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
     }
 }
