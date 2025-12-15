150e1b52c18beb953903fbdffda21324d095f791
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-15 14:52:53 +0200

    Add project files.

---
+++ GymManagementDAL/Entities/GymUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    internal class GymUser : BaseEntity
+    {
+        public string Name { get; set; } = null;
+        public string Email { get; set; } = null;
+    }
+}
