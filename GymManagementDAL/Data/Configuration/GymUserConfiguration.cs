a25ca043a2a935e91e0809bed5ca6b6347c755d3
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-16 14:44:03 +0200

    Make the GymUserConfiguration to make Constraints on the GymUser Class.

---
+++ GymManagementDAL/Data/Configuration/GymUserConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using GymManagementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Data.Configuration
+{
+    internal class GymUserConfiguration<T> : IEntityTypeConfiguration<T> where T : GymUser
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.Id)
+                .IsRequired();
+
+            builder.Property(x => x.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+
+            builder.Property(x=>x.Email)
+                .HasColumnType("varchar")
+                .HasMaxLength(100);
+
+            builder.Property(x=>x.Phone)
+                .HasColumnType("varchar")
+                .HasMaxLength(11);
+
+            builder.ToTable(Tb =>
+            {
+                Tb.HasCheckConstraint("GymUserCheckValidation", "Email Like '_%@_%._%'");
+                Tb.HasCheckConstraint("GymUserPhoneCheckValidation", "Phone Like '01%' and Phone not Like '[^0-9]%'");
+            });
+            builder.HasIndex(x => x.Email).IsUnique();
+            builder.HasIndex(x => x.Phone).IsUnique();
+
+            builder.OwnsOne(x => x.Address, AddressBuilder =>
+            {
+                AddressBuilder.Property(a=>a.Street)
+                .HasColumnName("Street")
+                .HasColumnType("varchar")
+                .HasMaxLength(30);
+
+                AddressBuilder.Property(a => a.City)
+                .HasColumnName("City")
+                .HasColumnType("varchar")
+                .HasMaxLength(30);
+
+                AddressBuilder.Property(a => a.BuildingNumber)
+                .HasColumnName("BuildingNumber");
+
+            });
+        }
+    }
+}
---
+++ GymManagementDAL/Data/Configuration/MemberConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Data.Configuration
+{
+    internal class MemberConfiguration : IEntityTypeConfiguration<Member>
+    {
+        public void Configure(EntityTypeBuilder<Member> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
