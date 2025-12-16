18f52d9f5037d21d373679dfe365e801f05d314b
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-16 12:35:00 +0200

    Add the Specialties Enum to use it inside the Trainer Class.

---
+++ GymManagementDAL/Entities/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementDAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    internal class Trainer
+    {
+        // HireDate == CreatedAt of BaseEntity.
+        public Specialties Specialties { get; set; }
+    }
+}
