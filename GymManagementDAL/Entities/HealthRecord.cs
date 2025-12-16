f60d6f2cf0ea7c5d444e5a65c3fca8b7b96597b2
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-16 12:29:33 +0200

    Add The HealthRecord Class to the Entities.

---
+++ GymManagementDAL/Entities/HealthRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    internal class HealthRecord : BaseEntity
+    {
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public string Blood { get; set; } = null!;
+        public string? Notes { get; set; } = null!;
+        
+
+    }
+}
