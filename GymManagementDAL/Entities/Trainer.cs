43da1d380470f211d7654fdbc79931beb384fbbe
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-16 14:57:14 +0200

    Add The TrainerConfiguration to implement from GymUserConfiguration<Trainer>, IEntityTypeConfiguration<Trainer> and make configuration inside it.

--- GymManagementDAL/Entities/Trainer.cs
+++ GymManagementDAL/Entities/Trainer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GymManagementDAL.Entities
 {
-    internal class Trainer
+    internal class Trainer : GymUser
     {
         // HireDate == CreatedAt of BaseEntity.
         public Specialties Specialties { get; set; }
