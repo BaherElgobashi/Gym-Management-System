cb6c43500ad41433bc4ca156bf047d406a4a9236
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-19 22:09:53 +0200

    Add signature to the IMemberService by using MemberViewModel.

--- GymManagementBL/Services/Interfcaes/IMemberService.cs
+++ GymManagementBL/Services/Interfcaes/IMemberService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GymManagementBLL.ViewModels.MemberViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace GymManagementBLL.Services.Interfcaes
 {
     internal interface IMemberService
     {
+        // GetAll.
+        IEnumerable<MemberViewModel> GetAllMembers();
     }
 }
