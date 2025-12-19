73f43878c40c88837511fb921e91c13ed3997f10
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-19 20:57:58 +0200

    Add IPlanRepositoryand Add PlanRepositoryto implement the IPlanRepositoryinside it.

---
+++ GymManagementDAL/Repositories/Classes/PlanRepository.cs
@@ -0,0 +1,39 @@
+﻿using GymManagementDAL.Contexts;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Classes
+{
+    internal class PlanRepository : IPlanRepository
+    {
+        private readonly GymDbContext _dbContext;
+        public PlanRepository(GymDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Plan> GetAll()
+        {
+            var plans = _dbContext.Plans.ToList();
+            return plans;
+        }
+
+        public Plan? GetById(int Id)
+        {
+            var plan = _dbContext.Plans.Find(Id);
+
+            return plan;
+        }
+
+        public int Update(Plan plan)
+        {
+            _dbContext.Plans.Update(plan);
+
+            return _dbContext.SaveChanges();
+        }
+    }
+}
