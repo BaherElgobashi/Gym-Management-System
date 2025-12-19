247eb9d54fb187b6ae26e1b2cda398f5f2042bba
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-19 20:52:17 +0200

    Add ISessionRepositoryand Add SessionRepositoryto implement the ISessionRepositoryinside it.

---
+++ GymManagementDAL/Repositories/Classes/SessionRepository.cs
@@ -0,0 +1,52 @@
+﻿using GymManagementDAL.Contexts;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Classes
+{
+    internal class SessionRepository : ISessionRepository
+    {
+        private readonly GymDbContext _dbContext;
+        public SessionRepository(GymDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Session> GetAll()
+        {
+            var sessions = _dbContext.Sessions.ToList();
+            return sessions;
+        }
+
+        public Session GetById(int Id)
+        {
+            var session = _dbContext.Sessions.Find(Id);
+            return session;
+        }
+        public int Add(Session session)
+        {
+            _dbContext.Sessions.Add(session);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Session session)
+        {
+            _dbContext.Sessions.Update(session);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(int Id)
+        {
+            var session = _dbContext.Sessions.Find(Id);
+
+            if (session is null) return 0;
+            _dbContext.Sessions.Remove(session);
+
+            return _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/ISessionRepository.cs
@@ -0,0 +1,27 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface ISessionRepository
+    {
+        // GetAll.
+        IEnumerable<Session> GetAll();
+
+        // GetById.
+        Session? GetById(int Id);
+
+        // Add.
+        int Add(Session session);
+
+        // Update.
+        int Update(Session session);
+
+        // Delete.
+        int Delete(int Id);
+    }
+}
