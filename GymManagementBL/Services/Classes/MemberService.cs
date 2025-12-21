f5c6ddbb67cb13e9fbb54d44a8170375936929c0
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-21 15:15:52 +0200

    Add MemberViewModel to make Manual Mapping inside the MemberService that implement the Interface IMemberService.

--- GymManagementBL/Services/Classes/MemberService.cs
+++ GymManagementBL/Services/Classes/MemberService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GymManagementBLL.Services.Interfcaes;
+using GymManagementBLL.ViewModels.MemberViewModels;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,56 @@ using System.Threading.Tasks;
 
 namespace GymManagementBLL.Services.Classes
 {
-    internal class MemberService
+    internal class MemberService : IMemberService
     {
+        private readonly IGenericRepository<Member> _memberRepository;
+
+        public MemberService(IGenericRepository<Member> memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+        public IEnumerable<MemberViewModel> GetAllMembers()
+        {
+            var Members = _memberRepository.GetAll();
+            if(Members is null || !Members.Any()){
+                return  Enumerable.Empty<MemberViewModel>();
+            }
+
+            #region Manual Mapping Way01
+            //var MemberViewModel = new List<MemberViewModel>();
+            //foreach (var Member in Members) 
+            //{
+            //    var memberviewmodel = new MemberViewModel() 
+            //    {
+            //        Id = Member.Id,
+            //        Name = Member.Name,
+            //        Photo = Member.Photo,
+            //        Email = Member.Email,
+            //        PhoneNumber = Member.Phone,
+            //        Gender = Member.Gender.ToString(),
+
+            //    };
+            //    MemberViewModels.Add(memberviewmodel);
+
+            //} 
+            #endregion
+
+            #region Manual Mapping Way02
+
+            var MemberViewModels = Members.Select(x => new MemberViewModel 
+            {   
+                Id = x.Id,
+                Name = x.Name,
+                Photo = x.Photo,
+                Phone = x.Phone,
+                Gender = x.Gender.ToString(),
+
+            });
+
+
+            #endregion
+            return MemberViewModels;
+            
+        }
     }
 }
--- GymManagementBL/ViewModels/MemberViewModels/MemberViewModel.cs
+++ GymManagementBL/ViewModels/MemberViewModels/MemberViewModel.cs
@@ -13,7 +13,7 @@ namespace GymManagementBLL.ViewModels.MemberViewModels
         public string Name { get; set; } = null!;
         public string? Photo { get; set; }
         public string Email { get; set; } = null!;
-        public string PhoneNumber { get; set; } = null!;
+        public string Phone { get; set; } = null!;
         public string Gender { get; set; } = null!;
     }
 }
