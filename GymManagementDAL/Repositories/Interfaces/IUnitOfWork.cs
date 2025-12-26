aa1ce73bcb3c38df1dadfe86bc2ca9ce32666637
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-26 14:08:40 +0200

    Add The IUnitOfWork in the Interfaces at the Data Access Layer Project.

---
+++ GymManagementDAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IUnitOfWork
+    {
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity , new();
+
+        int SaveChanges();
+    }
+}
