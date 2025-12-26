31ac4cf87f8fb117bd28d382158147fe7340e713
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-26 15:28:51 +0200

    Add The UpdatePlanViewModel and Add Constraints inside it using Data Annotation.

---
+++ GymManagementBL/Services/Interfcaes/IPlanService.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementBLL.ViewModels.PlanViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Interfcaes
+{
+    internal interface IPlanService
+    {
+        // Get All Plans.
+        IEnumerable<PlanViewModel>GetAllPlans();
+
+        // Get Details of one plan only by using Id.
+        PlanViewModel? GetPlanById(int PlanId);
+
+        //
+    }
+}
---
+++ GymManagementBL/ViewModels/PlanViewModels/UpdatePlanViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.ViewModels.PlanViewModels
+{
+    internal class UpdatePlanViewModel
+    {
+        [Required(ErrorMessage ="Plan Name Is Required.")]
+        [StringLength(50 , ErrorMessage ="Plan Name must be less than 51 Charactres.")]
+        public string PlanName { get; set; } = null!;
+
+        ////////////////////////////////////////////////////////////////////////////////
+
+        [Required(ErrorMessage ="Description is Required.")]
+        [StringLength(200, MinimumLength = 5 , ErrorMessage = "Description must be between 5 and 200 Characters.")]
+        public string Description { get; set; } = null!;
+        ////////////////////////////////////////////////////////////////////////////////
+
+        [Required(ErrorMessage ="Duration Days Is Required.")]
+        [Range(1,365, ErrorMessage = "Duration Days must be between 1 and 365 Day.")]
+        public int DurationDays { get; set; }
+        ////////////////////////////////////////////////////////////////////////////////
+
+        [Required(ErrorMessage ="Price Is Required.")]
+        [Range(0.1 , 1000 , ErrorMessage = "Price must be between 0.1 and 1000 in Price.")]
+        public decimal Price { get; set; }
+    }
+}
