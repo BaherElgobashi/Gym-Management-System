d1539c0317c04ffd6da4b5ef2214a1aa3d851538
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-28 15:03:19 +0200

    Add TrainerService and Implement ITrainerService, the make the implementation of GetAllTrainers Signature inside it.

---
+++ GymManagementBL/Services/Classes/TrainerService.cs
@@ -0,0 +1,39 @@
+﻿using GymManagementBLL.Services.Interfcaes;
+using GymManagementBLL.ViewModels.TrainerViewModels;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    internal class TrainerService : ITrainerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TrainerService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IEnumerable<TrainerViewModel> GetAllTrainers()
+        {
+            var Trainers = _unitOfWork.GetRepository<Trainer>().GetAll();
+
+            if (Trainers == null || !Trainers.Any())
+                return [];
+
+            var TrainerViewModels = Trainers.Select(x=>new TrainerViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email,
+                Phone = x.Phone,
+                Specialization = x.Specialties.ToString()
+            });
+            return TrainerViewModels;
+        }
+    }
+}
