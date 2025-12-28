60a89f0908221199a929d3f6280767baa8cc5ecb
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-28 14:56:47 +0200

    Add ITrainerService and Add GetAllTrainers Signature inside it.

---
+++ GymManagementBL/Services/Interfcaes/ITrainerService.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementBLL.ViewModels.TrainerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Interfcaes
+{
+    internal interface ITrainerService
+    {
+        // Get All Trainers.
+        IEnumerable<TrainerViewModel> GetAllTrainers();
+    }
+}
