83805b00e1e9eae5222154e358f25c9998b8ac50
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-01-03 14:18:37 +0200

    Reactor The ISessionRepository to add ne signatures and Make it inherit from IGenericRepository.

---
+++ GymManagementBL/Services/Classes/SessionService.cs
@@ -0,0 +1,18 @@
+﻿using GymManagementBLL.Services.Interfcaes;
+using GymManagementBLL.ViewModels.SessionViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    internal class SessionService : ISessionService
+    {
+        public IEnumerable<SessionViewModel> GetAllSessions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GymManagementDAL/Repositories/Interfaces/ISessionRepository.cs
+++ GymManagementDAL/Repositories/Interfaces/ISessionRepository.cs
@@ -7,21 +7,28 @@ using System.Threading.Tasks;
 
 namespace GymManagementDAL.Repositories.Interfaces
 {
-    internal interface ISessionRepository
+    internal interface ISessionRepository : IGenericRepository<Session>
     {
-        // GetAll.
-        IEnumerable<Session> GetAll();
+        #region Inherited These Signatures from Genrice Repository
+        //// GetAll.
+        //IEnumerable<Session> GetAll();
 
-        // GetById.
-        Session? GetById(int Id);
+        //// GetById.
+        //Session? GetById(int Id);
 
-        // Add.
-        int Add(Session session);
+        //// Add.
+        //int Add(Session session);
 
-        // Update.
-        int Update(Session session);
+        //// Update.
+        //int Update(Session session);
 
-        // Delete.
-        int Delete(int Id);
+        //// Delete.
+        //int Delete(int Id); 
+        #endregion
+
+
+        IEnumerable<Session> GetAllSessionsWithTrainerAndCategory();
+
+        int GetCountOfBookedSlots(int SessionId);
     }
 }
