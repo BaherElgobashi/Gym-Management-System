5789a2c83fe250f7f21711f4b8fa9c3fb981972b
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-01-03 14:08:55 +0200

    Add ISessionService and Add GetAllSessions Signature inside it.

---
+++ GymManagementBL/Services/Interfcaes/ISessionService.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementBLL.ViewModels.SessionViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Interfcaes
+{
+    public interface ISessionService
+    {
+        // Get All Sessions.
+        IEnumerable<SessionViewModel> GetAllSessions();
+    }
+}
