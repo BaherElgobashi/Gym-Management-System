3b144e15ff00de24cddf91b7615065e0bf03ba9b
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-01-04 14:40:01 +0200

    Make MappingProfiles inherit from Profile to use AutoMapper inside it to convert from Session to SessionViewModel.

---
+++ GymManagementBL/MappingProfiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GymManagementBLL.ViewModels.SessionViewModels;
+using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL
+{
+    internal class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Session, SessionViewModel>()
+                .ForMember(dest => dest.CategoryName, Options =>
+                {
+                    Options.MapFrom(src => src.SessionCategory.CategoryName);
+                })
+                .ForMember(dest => dest.TrainerName, Options =>
+                {
+                    Options.MapFrom(src => src.SessionTrainer.Name);
+                })
+                .ForMember(dest => dest.AvaliableSlots, Options =>
+                {
+                    Options.Ignore();
+                });
+        }
+    }
+}
