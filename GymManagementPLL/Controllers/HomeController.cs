307756fbb4c166da9c6fc0068f8aba3ec102f287
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-01-07 15:23:53 +0200

    Add style.css file to the css folder.

--- GymManagementPLL/Controllers/HomeController.cs
+++ GymManagementPLL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
 {
@@ -7,6 +8,22 @@ namespace GymManagementPL.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Trainers()
+        {
+            var Trainers = new List<Trainer>() 
+            {
+             new Trainer(){Name = "Baher",Phone = "01113411677"},
+             new Trainer(){Name = "Asser",Phone = "01556564970"},
+
+            };
+            return Json(Trainers);
+        }
+
+        public ContentResult Content()
+        {
+            return Content("Helllo  From iSchool");
         }
     }
 }
