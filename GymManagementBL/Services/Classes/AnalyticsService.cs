ffd91539f598f59cbd1ca01ff397136fc639706c
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-01-08 13:08:23 +0200

    Implement The GetAnalyticsData inside the AnalyticsService and use the AnalyticsViewModel to Make it.

---
+++ GymManagementBL/Services/Classes/AnalyticsService.cs
@@ -0,0 +1,44 @@
+﻿using GymManagementBLL.Services.Interfcaes;
+using GymManagementBLL.ViewModels.AnalyticsViewModels;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class AnalyticsService : IAnalyticsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AnalyticsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public AnalyticsViewModel GetAnalyticsData()
+        {
+
+            var Sessions = _unitOfWork.SessionRepository.GetAll();
+
+            return new AnalyticsViewModel() 
+            {
+                ActiveMembers = _unitOfWork.GetRepository<Membership>().GetAll(x=>x.Status == "Active").Count(),
+
+                TotalMembers = _unitOfWork.GetRepository<Member>().GetAll().Count(),
+
+                TotalTrainers = _unitOfWork.GetRepository<Trainer>().GetAll().Count(),
+
+                UpcomingSessions = Sessions.Count(x => x.StartDate > DateTime.Now),
+
+                OngoingSessions = Sessions.Count(x => x.StartDate <= DateTime.Now && x.EndDate >= DateTime.Now),
+
+                CompletedSessions = Sessions.Count(x => x.EndDate < DateTime.Now),
+
+            
+            };
+        }
+    }
+}
