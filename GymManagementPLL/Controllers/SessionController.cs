9bdf6e0720ad3374d421e1b17f291f34765ac90a
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-02-06 19:35:17 +0200

    Add Details as an Action inside the SessionController.

--- GymManagementPLL/Controllers/SessionController.cs
+++ GymManagementPLL/Controllers/SessionController.cs
@@ -12,11 +12,41 @@ namespace GymManagementPL.Controllers
             _sessionService = sessionService;
         }
 
+        #region Get All Plans.
+
         public IActionResult Index()
         {
             var Sessions = _sessionService.GetAllSessions();
 
             return View(Sessions);
         }
+
+        #endregion
+
+        #region Get Session Details.
+
+        public IActionResult Details(int id)
+        {
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid ID.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var Session = _sessionService.GetSessionById(id);
+            if (Session == null) 
+            {
+                TempData["ErrorMessage"] = "Session is Not Found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Session);
+
+        }
+
+
+        #endregion
+
+
+
     }
 }
