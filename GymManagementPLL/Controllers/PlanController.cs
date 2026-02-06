d885c0ad272cda1d9c1892fd231d70bbfb58063d
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-02-06 15:15:33 +0200

    Add the Details as an Action in the PlanController.

--- GymManagementPLL/Controllers/PlanController.cs
+++ GymManagementPLL/Controllers/PlanController.cs
@@ -28,5 +28,33 @@ namespace GymManagementPL.Controllers
 
         #endregion
 
+        #region Get Plan Details.
+
+        public IActionResult Details(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id can't be Zero or Negative Number.";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            var Plan = _planService.GetPlanById(id);
+            if (Plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan is Not Found.";
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(Plan);
+
+
+
+            #endregion
+
+        }
+
     }
 }
+        
+    
