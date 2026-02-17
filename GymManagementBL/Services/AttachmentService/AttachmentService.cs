a43eb390bc527a49d2f1c65178add0adf2b16e82
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-02-17 13:22:37 +0200

    Add AttachmentService as a class then implement the IAttachmentService Interface inside it.

---
+++ GymManagementBL/Services/AttachmentService/AttachmentService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.AttachmentService
+{
+    public class AttachmentService : IAttachmentService
+    {
+        public string? Upload(string folderName, IFormFile File)
+        {
+            throw new NotImplementedException();
+        }
+    
+        public bool Delete(string folderName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
