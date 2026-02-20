4be7c92d72b5c6e0439326708e7308e028f80bce
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-02-20 15:17:40 +0200

    Add AccountService then make it implement the IAccountService interface.

---
+++ GymManagementBL/Services/Classes/AccountService.cs
@@ -0,0 +1,37 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.AccountViewModels;
+using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountService(UserManager<ApplicationUser>userManager)
+        {
+            _userManager = userManager;
+        }
+        public ApplicationUser? ValidateUser(LoginViewModel loginViewModel)
+        {
+            var User = _userManager.FindByEmailAsync(loginViewModel.Email).Result;
+
+            if (User is null)
+                return null;
+
+            var IsPassword = _userManager.CheckPasswordAsync(User , loginViewModel.Password).Result;
+
+            return IsPassword ? User : null;    
+            
+
+
+        }
+    }
+}
