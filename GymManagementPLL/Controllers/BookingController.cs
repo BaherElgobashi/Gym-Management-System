77da5988301f93a4eb2caec02cbc6bf44a7edaa5
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 22:12:32 +0300

    Implement all Actions inside the BookingController.

--- GymManagementPLL/Controllers/BookingController.cs
+++ GymManagementPLL/Controllers/BookingController.cs
@@ -1,5 +1,8 @@
-﻿using GymManagementBLL.Services.Interfaces;
+﻿using GymManagementBLL.Services.Classes;
+using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.BookingViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GymManagementPL.Controllers
 {
@@ -15,6 +18,77 @@ namespace GymManagementPL.Controllers
         {
             var sessions = _bookingService.GetAllSessionsWithTrainerAndCategory(); 
             return View(sessions);
+        }
+
+        public IActionResult GetMembersForUpcomingSession(int id)
+        {
+            var members = _bookingService.GetAllMembersForSession(id);
+            return View(members);
+        }
+        public IActionResult GetMembersForOngoingSession(int id)
+        {
+            var members = _bookingService.GetAllMembersForSession(id);
+            return View(members);
+        }
+
+
+        public IActionResult Create(int id)
+        {
+            var members = _bookingService.GetMemberForDropdown(id);
+            var membersSelectList = new SelectList(members, "Id", "Name");
+            ViewBag.Members = membersSelectList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateBookingViewModel model)
+        {
+            var result = _bookingService.CreateBooking(model);
+
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Booking created successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Booking failed. Member must have an active membership.";
+            }
+
+            return RedirectToAction(nameof(GetMembersForUpcomingSession), new { id = model.SessionId });
+        }
+
+
+        [HttpPost]
+        public IActionResult Attended(MemberAttendOrCancelViewModel model)
+        {
+            var result = _bookingService.MemberAttended(model);
+
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Member attended successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Member attendance can't be marked";
+            }
+            return RedirectToAction(nameof(GetMembersForOngoingSession), new { id = model.SessionId });
+        }
+
+        [HttpPost]
+        public IActionResult Cancel(MemberAttendOrCancelViewModel model)
+        {
+            var result = _bookingService.CancelBooking(model);
+
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Booking cancelled successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Booking can't be cancelled";
+            }
+            return RedirectToAction(nameof(GetMembersForUpcomingSession), new { id = model.SessionId });
         }
     }
 }
