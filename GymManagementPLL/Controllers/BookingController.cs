e58e78b1071e19a2d071f44b2c2b384192f785f9
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 22:05:53 +0300

    Add BookingController. Make dependency injection to use booking service.

---
+++ GymManagementPLL/Controllers/BookingController.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
