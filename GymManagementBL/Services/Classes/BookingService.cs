2711fbebf4b134c3e15dc996da5fc37c986dbaa6
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 15:19:05 +0300

    Add new service inside the Program.cs file between IBookingService and BookingService to allow the dependency Injection.

---
+++ GymManagementBL/Services/Classes/BookingService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.BookingViewModels;
+using GymManagementBLL.ViewModels.MemberShipViewModel;
+using GymManagementBLL.ViewModels.SessionViewModels;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class BookingService : IBookingService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public BookingService(IUnitOfWork unitOfWork , IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            
+            _mapper = mapper;
+        }
+
+
+        public IEnumerable<SessionViewModel> GetAllSessionsWithTrainerAndCategory()
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<MemberForSessionViewModel> GetAllMembersForSession(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MemberForSelectListViewModel> GetMemberForDropdown(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public bool CancelBooking(MemberAttendOrCancelViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateBooking(CreateBookingViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public bool MemberAttended(MemberAttendOrCancelViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GymManagementPLL/Program.cs
+++ GymManagementPLL/Program.cs
@@ -65,6 +65,8 @@ namespace GymManagementPLL
 
             builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 
+            builder.Services.AddScoped<IBookingService, BookingService>();
+
 
 
             builder.Services.AddIdentity<ApplicationUser , IdentityRole>(Config =>
