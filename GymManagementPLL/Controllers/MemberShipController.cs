c14165cb29eb28b85c80d6be59accf721af88bf4
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 12:32:42 +0300

    Add MemberShipController and Inplement The Index Action to Get all MemberShips.

---
+++ GymManagementPLL/Controllers/MemberShipController.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        private readonly IMemberShipService _memberShipService;
+
+        public MemberShipController(IMemberShipService memberShipService)
+        {
+            _memberShipService = memberShipService;
+        }
+        public IActionResult Index()
+        {
+            var MemberShips = _memberShipService.GetAllMemberShips();
+            return View(MemberShips);
+        }
+    }
+}
