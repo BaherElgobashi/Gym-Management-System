16ff46f7cfd8cb011557381e07ba4bc4f57abeff
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 15:02:41 +0300

    Add IBookingService and Add GetAllSessionsWithTrainerAndCategory as a new signature inside it.

---
+++ GymManagementBL/Services/Interfaces/IBookingService.cs
@@ -0,0 +1,14 @@
+﻿using GymManagementBLL.ViewModels.SessionViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Interfaces
+{
+    public interface IBookingService
+    {
+        IEnumerable<SessionViewModel> GetAllSessionsWithTrainerAndCategory();
+    }
+}
