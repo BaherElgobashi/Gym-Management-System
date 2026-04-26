d4c178847aa7e6e9e5c262025e0d21b073193ce4
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-26 14:36:12 +0300

    Add BookingRepository and make it implement IBookingTRepository and inherit from GenericRepository<MemberSession>.

---
+++ GymManagementDAL/Repositories/Classes/BookingRepository.cs
@@ -0,0 +1,26 @@
+﻿using GymManagementDAL.Contexts;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Classes
+{
+    public class BookingRepository : GenericRepository<MemberSession>, IBookingRepository
+    {
+        private readonly GymDbContext _dbContext;
+
+        public BookingRepository( GymDbContext dbContext):base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        MemberSession? IGenericRepository<MemberSession>.GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
